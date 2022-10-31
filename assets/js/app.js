85bdf27c54e230a3db558b2c7bf7ae80511b3a8a
Bret Andersen <1bretly@gmail.com>
2022-10-31 11:46:44 -0500

    hide play button on click, startGame function is called

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// const for the play button with aEL to call startGame function
+let playBtn = document.getElementById("play-btn")
+playBtn.addEventListener("click", startGame)
+
+function startGame() {
+  playBtn.style.display = "none"
+}
+;``
