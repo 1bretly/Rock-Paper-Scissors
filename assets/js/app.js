e028ec60e40d327f90af202e6f122c33c8440b69
Bret Andersen <1bretly@gmail.com>
2022-10-31 12:24:45 -0500

    added rpsButtons to log user input and appended them to page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,16 @@
-// const for the play button with aEL to call startGame function
+// let for the possible choices of rock, paper, or scissors
+let choices = ["Rock", "Paper", "Scissors"]
+
+// let for the play button with aEL to call startGame function
 let playBtn = document.getElementById("play-btn")
 playBtn.addEventListener("click", startGame)
 
 function startGame() {
   playBtn.style.display = "none"
+  for (let i = 0; i < choices.length; i++) {
+    let rpsButtons = document.createElement("button")
+    rpsButtons.innerHTML = choices[i]
+    rpsButtons.id = "rpsButtons"
+    document.body.appendChild(rpsButtons)
+  }
 }
-;``
