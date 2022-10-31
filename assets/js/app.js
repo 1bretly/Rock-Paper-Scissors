10f0ff70e91f05cc3db017b5547be922afd7e9e1
Bret Andersen <1bretly@gmail.com>
2022-10-31 16:03:06 -0500

    created a let to store the input of the user

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,50 @@
 // let for the possible choices of rock, paper, or scissors
 let choices = ["Rock", "Paper", "Scissors"]
 
+// empty let for user/cpuChoice
+let userChoice
+let cpuChoice
+
 // let for the play button with aEL to call startGame function
 let playBtn = document.getElementById("play-btn")
 playBtn.addEventListener("click", startGame)
 
 function startGame() {
   playBtn.style.display = "none"
+
   for (let i = 0; i < choices.length; i++) {
     let rpsButtons = document.createElement("button")
     rpsButtons.innerHTML = choices[i]
-    rpsButtons.id = "rpsButtons"
+    rpsButtons.className = "rpsButtons"
+    rpsButtons.id = choices[i]
     document.body.appendChild(rpsButtons)
+    rpsButtons.addEventListener("click", function (e) {
+      if (rpsButtons.id === "Rock") {
+        userChoice = "Rock"
+      }
+      if (rpsButtons.id === "Paper") {
+        userChoice = "Paper"
+      }
+      if (rpsButtons.id === "Scissors") {
+        userChoice = "Scissors"
+      }
+      console.log(userChoice)
+    })
+    // cpuInput()
   }
 }
+
+// function cpuInput() {
+//   const randomNum = Math.floor(Math.random() * 3) + 1
+//   if (randomNum === 1) {
+//     cpuChoice = "rock"
+//   }
+//   if (randomNum === 2) {
+//     cpuChoice = "paper"
+//   }
+//   if (randomNum === 3) {
+//     cpuChoice = "scissors"
+//   }
+//   console.log(cpuChoice)
+//   console.log("hi")
+// }
