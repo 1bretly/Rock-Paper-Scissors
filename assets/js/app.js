49ccddd30aed333ccee5b810794cf3b5d66cd916
Bret Andersen <1bretly@gmail.com>
2022-10-31 18:27:08 -0500

    function to log cpu input

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,20 @@
 // let for the possible choices of rock, paper, or scissors
 let choices = ["Rock", "Paper", "Scissors"]
 
-// empty let for user/cpuChoice
+// empty let for user that we will log a value for
 let userChoice
-let cpuChoice
+
+// const/let for keeping track of score
+let wins = 0
+let losses = 0
+let ties = 0
 
 // let for the play button with aEL to call startGame function
 let playBtn = document.getElementById("play-btn")
 playBtn.addEventListener("click", startGame)
 
+// startGame function to hide the play button and create new buttons for each choice: Rock, Paper, or Scissors
+// also assign the buttons a class and individual id's
 function startGame() {
   playBtn.style.display = "none"
 
@@ -21,30 +27,56 @@ function startGame() {
     rpsButtons.addEventListener("click", function (e) {
       if (rpsButtons.id === "Rock") {
         userChoice = "Rock"
+        cpuInput()
       }
       if (rpsButtons.id === "Paper") {
         userChoice = "Paper"
+        cpuInput()
       }
       if (rpsButtons.id === "Scissors") {
         userChoice = "Scissors"
+        cpuInput()
       }
       console.log(userChoice)
     })
-    // cpuInput()
   }
 }
 
-// function cpuInput() {
-//   const randomNum = Math.floor(Math.random() * 3) + 1
-//   if (randomNum === 1) {
-//     cpuChoice = "rock"
-//   }
-//   if (randomNum === 2) {
-//     cpuChoice = "paper"
-//   }
-//   if (randomNum === 3) {
-//     cpuChoice = "scissors"
-//   }
-//   console.log(cpuChoice)
-//   console.log("hi")
-// }
+function cpuInput() {
+  let cpuChoice = choices[Math.floor(Math.random() * choices.length)]
+  console.log(cpuChoice)
+  if (cpuChoice === userChoice) {
+    ties++
+    displayScore()
+  }
+  if (cpuChoice === "Rock" && userChoice === "Paper") {
+    wins++
+    displayScore()
+  }
+  if (cpuChoice === "Rock" && userChoice === "Scissors") {
+    losses++
+    displayScore()
+  }
+  if (cpuChoice === "Paper" && userChoice === "Rock") {
+    losses++
+    displayScore()
+  }
+  if (cpuChoice === "Paper" && userChoice === "Scissors") {
+    wins++
+    displayScore()
+  }
+  if (cpuChoice === "Scissors" && userChoice === "Rock") {
+    wins++
+    displayScore()
+  }
+  if (cpuChoice === "Scissors" && userChoice === "Paper") {
+    losses++
+    displayScore()
+  }
+}
+
+function displayScore() {
+  let score = document.createElement("div")
+  score.id = "score"
+  score.innerHTML = wins + " -" + losses + " -" + ties
+}
